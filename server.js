6ea7966999d2c6851b8a5b8a2bf0b275ff3332ec
DIONISIUS <141792935+Emanuele-Comand@users.noreply.github.com>
2024-02-16 14:32:13 +0100

    add ngrock and change server.js

--- server.js
+++ server.js
@@ -5,14 +5,22 @@ const fs = require("fs");
 
 app.use(express.static("public"));
 
-https
-  .createServer(
-    {
-      key: fs.readFileSync("./key.pem"),
-      cert: fs.readFileSync("./cert.pem"),
-    },
-    app
-  )
-  .listen(3000, () => {
-    console.log("Listening on https://localhost:3000");
-  });
+// app.get("/", (req, res) => {
+//   res.send(express.static("public"));
+// });
+
+app.listen(3000, () => {
+  console.log(`Example app listening on port 3000`);
+});
+
+// https
+//   .createServer(
+//     {
+//       key: fs.readFileSync("./key.pem"),
+//       cert: fs.readFileSync("./cert.pem"),
+//     },
+//     app
+//   )
+//   .listen(3000, () => {
+//     console.log("Listening on https://localhost:3000");
+//   });
